30dc2da147172cd2bf3005202e7055cb4e940b3a
AntociM <gidioimihaela@gmail.com>
2021-11-05 21:08:35 +0000

    Add event handleEvent for WeeklyLimit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,17 +43,29 @@ function handleSubmit(event) {
     // event.target.reset();
     updateDashboard();
 }
+
 /**
- * This function will compare the weekly-limit value set by the user with the curent weekly balance.
- * If expenses are greater then the limit set, the amount will be styled with red color.
+ * This function triggers on change event for weekly-limit HTML element
+ * @param {*} event 
  */
 function handleWeeklyLimit(event) {
-    
-    let  weeklyLimit =  event.target.value;
-    let  weeklyAmount =  parseFloat(document.getElementById("weekly-expenses").textContent);
+    event.preventDefault();
+    updateWeeklySpendingLimit();
+}
 
-    console.log(weeklyLimit);
-    console.log(weeklyAmount);
+/**
+ * This function will compare the weekly-limit value set by the user with the curent weekly balance
+ * If expenses are greater then the limit set, the amount will be styled with red color
+ */
+function updateWeeklySpendingLimit() {
+    let weeklyLimit = document.getElementById("weekly-limit").value;
+    let weeklyAmountElement = document.getElementById("weekly-expenses");
+    if (parseFloat(weeklyAmountElement.textContent) > weeklyLimit) {
+        weeklyAmountElement.className = "error";
+    }
+    else{
+        weeklyAmountElement.classList.remove("error");
+    }
 }
 
 // Add event listners for HTML elements
@@ -71,20 +83,13 @@ weeklyLimit.addEventListener("change", handleWeeklyLimit);
 function updateBalance() {
 
     let rows = document.getElementsByClassName("entry-row");
-
     let balance = 0;
-    
     for (let row of rows) {
         if (row.children[1].textContent === "income") {
-
-
             balance += parseFloat(row.children[3].textContent);
-
         } else {
-
             balance -= parseFloat(row.children[3].textContent);
         }
-        
     }
 
     let currentBalance = document.getElementById("balance");
@@ -94,79 +99,57 @@ function updateBalance() {
 /**
  * Updates the monthly balance with inputs that match the month and year.
  */
-
 function updateMonthlyBalance() {
-
     let rows = document.getElementsByClassName("entry-row");
-
     let monthlyAmount = 0;
     let today = new Date();
     let month = today.getUTCMonth();
     let year = today.getUTCFullYear();
 
     for (let row of rows) {
-
-
         let rowDate = new Date(row.children[2].textContent);
 
         if (month === rowDate.getUTCMonth() && year === rowDate.getUTCFullYear() && row.children[1].textContent !== "income") {
-
             monthlyAmount += parseFloat(row.children[3].textContent);
-
         }
-
     }
 
     let currentMonthlyBalance = document.getElementById("monthly-expenses");
     currentMonthlyBalance.textContent = monthlyAmount.toString();
-
 }
 
 /**
  * Updates balance, weekly and monthly amounts
  */
-
 function updateDashboard() {
     updateBalance();
     updateMonthlyBalance();
     currentWeeklyBalance();
+    updateWeeklySpendingLimit();
 }
+
 /**
  * This function iterates through entry-rows and extracts the inputs made in 
  * the same week of same year.  
  */
-
 function currentWeeklyBalance() {
-
     let rows = document.getElementsByClassName("entry-row");
-
     let weeklyAmount = 0;
     let today = new Date();
     let firstJan = new Date(today.getFullYear(), 0, 1);
     let numberOfDays = Math.floor((today - firstJan) / (24 * 60 * 60 * 1000));
     let weekNr = Math.ceil((today.getDay() + 1 + numberOfDays) / 7);
 
-
     for (let row of rows) {
-
         let rowDate = new Date(row.children[2].textContent);
         let rowFirstJan = new Date(rowDate.getFullYear(), 0, 1);
         let rowNumberOfDays = Math.floor((rowDate - rowFirstJan) / (24 * 60 * 60 * 1000));
         let rowWeekNr = Math.ceil((rowDate.getDay() + 1 + rowNumberOfDays) / 7);
-
-
-
-
         if (weekNr === rowWeekNr && today.getFullYear() === rowDate.getFullYear() && row.children[1].textContent !== "income") {
             weeklyAmount += parseFloat(row.children[3].textContent);
         }
-
     }
+
     let currentWeeklyBalance = document.getElementById("weekly-expenses");
     currentWeeklyBalance.textContent = weeklyAmount.toString();
-
 }
-
-
-
-
